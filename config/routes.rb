a72ca5bd56e019e71adacca07d0187631562647c
narshlob <traceyeubanks@yahoo.com>
2010-03-08 21:31:54 -0700

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resource :account, :controller => "users"
+  map.resources :users
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
