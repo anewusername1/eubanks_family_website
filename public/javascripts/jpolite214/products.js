df113c298f9d3901641170486ae918900a566eca
narshlob <traceyeubanks@yahoo.com>
2010-04-06 06:28:08 -0600

    added jquery and removed prototype

---
+++ public/javascripts/jpolite214/products.js
@@ -0,0 +1,11 @@
+[ { "name" : "Product",
+    "title" : "Alienware",
+    "type" : "Member",
+    "url" : "products1.js"
+  },
+  { "name" : "Product",
+    "title" : "Macbook Pro",
+    "type" : "Member",
+    "url" : "products2.js"
+  }
+]
