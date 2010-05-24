75edaed99bf3f3cf70bc755d7e846e315b38f9bd
narshlob <traceyeubanks@yahoo.com>
2010-05-23 22:35:18 -0600

    created albums and made them work. Skeleton of website is nearing completiongit status

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,19 @@
 class AlbumsController < ApplicationController
+  before_filter :require_user, :only => [:index, :create, :new, :show, :edit, :update, :destroy]
+
+  def index
+    @albums = Album.all(:conditions => {"user_id" => current_user.id})
+  end
+
+  def new
+    Album.new
+  end
+
+  def create
+    @album = Album.new(params[:album].merge(:user => current_user))
+    if(@album.save)
+      flash[:notice] = "Album created"
+      redirect_to album_url(@album.id)
+    end
+  end
 end
