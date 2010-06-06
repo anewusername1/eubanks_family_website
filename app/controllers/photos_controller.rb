2cea0b6f3a8f94e0d6c1a2461ca2b59f2baf5a88
narshlob <traceyeubanks@yahoo.com>
2010-06-06 10:53:52 -0600

    updated design doc. photos are closer to working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,7 @@ class PhotosController < ApplicationController
   before_filter :require_user
 
   def index
+    @album = Album.find(params[:album_id])
     @photos = Photo.all(:conditions => {"album_id" => params[:album_id]})
   end
 
@@ -10,16 +11,18 @@ class PhotosController < ApplicationController
   end
 
   def edit
+    @album = Album.find(params[:album_id])
     @photo = Photo.find(params[:id])
   end
 
   def show
+    @album = Album.find(params[:album_id])
     @photo = Photo.find(params[:id])
   end
 
   def update
     @photo = Photo.find(params[:id])
-    if @photo.update_attributes(params[:photo])
+    if @photo.update_attributes(params[:photo].merge({:album_id => params[:album_id]}))
       flash[:notice] = "Photo updated!"
       redirect_to album_photos_url(params[:album_id])
     else
