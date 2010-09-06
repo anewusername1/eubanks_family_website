d9304858c1fbcbfb83cd769b3c9f4f932fc02d72
narshlob <traceyeubanks@yahoo.com>
2010-09-05 18:03:08 -0600

    made it so photos will upload tons at a time. Started upgrading from 2.3.8 to 3.0.0

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,7 +7,8 @@ class PhotosController < ApplicationController
   end
 
   def new
-    @user = Photo.new
+    @album = Album.find(params[:album_id])
+    @photo = Photo.new
   end
 
   def edit
@@ -31,11 +32,40 @@ class PhotosController < ApplicationController
   end
 
   def create
-    @photo = Photo.new(params[:photo].merge({:album_id => params[:album_id]}))
-    if(@photo.save)
-      flash[:notice] = "Photo created"
-      redirect_to album_photos_url(params[:album_id])
+    if(params["photo"])
+      
+      @photo = Photo.new(params[:photo].merge({:album_id => params[:album_id]}))
+      if(@photo.save)
+        flash[:notice] = "Photo created"
+        redirect_to album_photos_url(params[:album_id])
+      end
     end
   end
 
 end
+if(params["image"])
+  @image_dir = "/u/apps/social_stats/shared/images"
+  new_file_name = "#{@image_dir}/#{params[:Filename]}"
+  FileUtils.mv(params["image"]["image"], new_file_name)
+  @image = Image.new({:name => params[:Filename], :location => "#{@image_dir}/#{params[:Filename]}"})
+  #@image.photo_content_type = MIME::Types.type_for(@image.photo_file_name).to_s
+  respond_to do |format|
+    if @image.save
+      flash[:notice] = 'Image was successfully created.'
+      #format.html { redirect_to(images_url) }
+      format.xml  { render :xml => @image, :status => :created, :location => @image }
+      format.json { render :json => { :result => 'success', :photo => images_url } }        
+      format.js   
+    else
+      format.html { render :action => "new" }
+      format.xml  { render :xml => @image.errors, :status => :unprocessable_entity }
+      format.json { render :json => { :result => 'error', :error => @image.errors.full_messages.to_sentence } }        
+      format.js   
+    end
+  end
+#   file_content_type = MIME::Types.type_for(@image.original_filename).to_s
+#   new_file_name = "#{RAILS_ROOT}/tmp/images.zip"
+#   FileUtils.mv(params["image"]["uploaded_data"], new_file_name)
+#   @image_dir = "/u/apps/social_stats/shared/images"
+#   
+#   redirect_to images_url
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,147 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  class NavigationTabs
+    def initialize(template)
+      @template = template
+    end
+    
+    def general_links(&block)
+      @template.concat("<div class='yui-u first'>\n")
+      yield
+      @template.concat("</div>\n")
+    end
+    
+    def system_links(&block)
+      @template.concat("<div class='yui-u'>\n")
+      yield
+      @template.concat("</div>\n")
+    end
+    
+    def link(text,url,options={})
+      html = ""
+      if (options[:root] && @template.request.request_uri == "/") || (@template.request.url.split("/")[3] == url.split("/")[3])
+        html << @template.content_tag("li", :class => "ui-state-default ui-corner-top ui-tabs-selected ui-state-active") do
+          @template.link_to text, url
+        end
+      else
+        html << @template.content_tag("li", :class => "ui-state-default ui-corner-top") do
+          @template.link_to text, url
+        end
+      end
+      return html
+    end
+  end
+  
+  class NamespaceNavigationTabs
+    def initialize(template)
+      @template = template
+    end
+    
+    def link(text,url)
+      html = ""
+      if "/#{@template.params[:controller]}" == url
+        html << @template.content_tag("li", :class => "ui-state-default ui-corner-top ui-tabs-selected ui-state-active") do
+          @template.link_to text, url
+        end
+      else
+        html << @template.content_tag("li", :class => "ui-state-default ui-corner-top") do
+          @template.link_to text, url
+        end
+      end
+      return html
+    end
+    
+  end
+  
+  class OptionRedirector
+    
+    def initialize(template)
+      @template = template
+    end
+    
+    def option(name,url)
+      "<option value='#{url}'>#{name}</option>" unless @template.request.request_uri.split('?').first == url
+    end
+    
+  end
+  
+  def namespace_navigation(&block)
+    @builder = NamespaceNavigationTabs.new(self)
+    concat("<div id='subnav' class='ui-tabs ui-widget'>\n")
+    concat("<ul class='ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header'>\n")
+    yield @builder
+    concat("</ul>\n")
+    concat("</div>\n")
+  end
+  
+  def navigation(&block)
+    @builder = NavigationTabs.new(self)
+    concat("<div id='nav' class='yui-ge ui-tabs ui-widget'>\n") # spaces to make output source readable
+    concat("<ul class='ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all'>\n")
+    yield @builder
+    concat("</ul>\n")
+    concat("</div>\n")
+  end
+
+  def navigation_for_namespace
+    if File.exists?("#{RAILS_ROOT}/app/views/#{params[:controller].split("/")[0]}/_navigation.html.haml")
+      render :partial => "#{params[:controller].split("/")[0]}/navigation"
+    end
+  end
+  
+  def include_form_scripts(exclude_form_js)
+    scripts = "jquery.relatedselects.js", "ui.multiselect.js", "ui.selectmenu.js", "ui.checkbox.js"
+    scripts << "form.js" unless exclude_form_js
+    content_for :script do
+      javascript_include_tag scripts
+    end
+    content_for :style do
+      stylesheet_link_tag "ui.multiselect.css", "ui.selectmenu.css", "ui.checkbox.css"
+    end
+  end
+  
+  def option_redirector(name,open=false,blank=nil,&block)
+    @builder = OptionRedirector.new(self)
+    include_form_scripts(true)
+    content_for :script do
+      unless open
+        javascript_tag do
+          %Q{
+            $(window).ready(function() {
+              $('.select:not(.multiselect)').selectmenu({
+            		maxHeight: 400,
+            		maxWidth: 200
+            	});
+              $('##{name}').change(function() {
+                if($('##{name} option:selected').attr('value') != '#{name}') {
+                  window.location = $('##{name} option:selected').attr('value');
+                }
+              });
+            });
+          }
+        end
+      else
+        javascript_tag do
+          %Q{
+            $(window).ready(function() {
+              $('.select:not(.multiselect)').selectmenu({
+            		maxHeight: 400,
+            		maxWidth: 200
+            	});
+              $('##{name}').change(function() {
+                if($('##{name} option:selected').attr('value') != '#{name}') {
+                  window.open($('##{name} option:selected').attr('value'));
+                }
+              });
+            });
+          }
+        end
+      end
+    end
+    concat("<select id='#{name}' class='select'>")
+    concat("<option>#{blank}</option>") if blank  
+    yield @builder
+    concat("</select>")
+  end
+  
 end
